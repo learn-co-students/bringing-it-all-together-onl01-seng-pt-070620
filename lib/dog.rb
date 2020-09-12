101d509175c54ef4d38d6f767129ba82ece0b258
asjames18 <asjames18@gmail.com>
2020-09-11 21:32:22 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,14 +41,12 @@ class Dog
       end
     end
 
+    def self.create(name, breed)
+        dog = Dog.new(name, breed)
+        dog.save
+        dog
+    end
 end
-
-    # def self.create(name, breed)
-    #     dog = Dog.new(name, breed)
-    #     dog.save
-    #     dog
-    # end
-
     # def self.new_from_db(row)
     #     dog = self.new(row[0], row[1], row[2])
     #     dog
