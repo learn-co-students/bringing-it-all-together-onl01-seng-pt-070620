e4f3c7ec693ea3173f4ac7c690c628cc3ce42b44
github username <github email address>
2020-09-15 16:50:40 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,9 +32,64 @@ class Dog
         self
     end 
 
-    def self.create(name, breed)
-        dog = Dog.new(name, breed)
+    def self.create(name:, breed:)
+        dog = self.new(name: name, breed: breed)
         dog.save
         dog
     end 
+
+    def self.new_from_db(row) 
+        name = row[1]
+        breed = row[2]
+        id = row[0]
+        self.new(id: id, name: name, breed: breed,)
+    end
+
+    def self.find_by_id(id)
+        sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE id = ?
+        SQL
+        DB[:conn].execute(sql,id).map do |row|
+            self.new_from_db(row)
+        end.first
+    end 
+
+    def self.find_or_create_by(name:, breed:)
+        sql = <<-SQL
+          SELECT * 
+          FROM dogs
+          WHERE name = ? 
+          AND breed = ?
+        SQL
+    
+    
+        dog = DB[:conn].execute(sql, name, breed).first
+    
+        if dog
+            new_dog = self.new_from_db(dog)
+        else
+            new_dog = self.create({:name => name, :breed => breed})
+        end
+        new_dog
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+          SELECT * FROM dogs WHERE name = ?
+        SQL
+    
+        DB[:conn].execute(sql, name).map do |row|
+          self.new_from_db(row)
+        end.first
+    end
+
+    def update
+        sql = <<-SQL
+          UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+          SQL
+    
+          DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
 end 
